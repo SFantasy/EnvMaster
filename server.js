92b01936b017b5b97f523d181ab1856cd593a279
Fantasy Shao <forever.fantasy27@gmail.com>
2017-03-05 20:27:42 +0800

    Change project settings (use webpack1)

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
+import express from 'express';
 import webpack from 'webpack';
-import WebpackDevServer from 'webpack-dev-server';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
 import { spawn } from 'child_process';
 
 import config from './webpack.dev.config';
 
+const app = express();
 const compiler = webpack(config);
+
 const PORT = 3000;
 
-const server = new WebpackDevServer(compiler, {
-  contentBase: '/app/',
-  publicPath: '/',
+const wdm = webpackDevMiddleware(compiler, {
+  publicPath: config.output.publicPath,
   stats: {
-    color: true,
-  },
+    colors: true
+  }
 });
 
-server.listen(PORT, () => {
+app.use(wdm);
+
+app.use(webpackHotMiddleware(compiler));
+
+const server = app.listen(PORT, err => {
+  if (err) {
+    console.log(err);
+  }
+
   spawn('npm', ['run', 'start-app'], { shell: true, env: process.env, stdio: 'inherit'})
     .on('close', code => process.exit(code))
     .on('error', spawnError => console.error(spawnError));
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,67 +1,62 @@
 // Development configuration
 
 import webpack from 'webpack';
+import path from 'path';
+
+const PORT = 3000;
 
 export default {
   devtool: 'inline-source-map',
 
   entry: [
+    `webpack-hot-middleware/client?path=http://localhost:${PORT}/__webpack_hmr`,
     'babel-polyfill',
     './app/index',
   ],
 
   output: {
+    path: path.join(__dirname, 'app'),
+    publicPath: `http://localhost:${PORT}/dist/`,
     filename: 'bundle.js',
   },
 
   module: {
-    rules: [
-      {
-        test: /\.js[x]?$/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        use: {
-          loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-        }
-      },
-      {
-        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        use: {
-          loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-        }
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        use: {
-          loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
-        }
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        use: {
-          loader: 'file-loader'
-        }
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        use: {
-          loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
-        }
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/,
-        use: {
-          loader: 'url-loader'
-        }
-      }
-    ]
+    loaders: [{
+      test: /\.js[x]?$/,
+      loaders: ['babel-loader'],
+      exclude: /node_modules/,
+    }, {
+      test: /\.scss$/,
+      loaders: ['style-loader', 'css-loader', 'sass-loader'],
+    }, {
+      test: /\.json$/,
+      loader: 'json-loader'
+    }, {
+      test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&mimetype=application/font-woff'
+    }, {
+      test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&mimetype=application/font-woff'
+    }, {
+      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&mimetype=application/octet-stream'
+    }, {
+      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'file'
+    }, {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&mimetype=image/svg+xml'
+    }, {
+      test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/,
+      loader: 'url-loader'
+    }],
   },
 
   plugins: [
-    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
   ]
 }
