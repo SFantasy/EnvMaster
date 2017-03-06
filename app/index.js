384797e981f703f4e5bb6cd67f1cfb98cb4ce28d
Fantasy Shao <forever.fantasy27@gmail.com>
2017-03-06 23:37:52 +0800

    Add react-router

--- app/index.js
+++ app/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import Dashboard from './Dashboard';
+import Layout from './Layout';
 
 render(
-  <Dashboard />,
+  <Router history={hashHistory}>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={Dashboard} />
+    </Route>
+  </Router>,
   document.getElementById('app')
 );
