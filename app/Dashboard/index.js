142a27e39783be55c11f47618783dac9831b6ae0
Fantasy Shao <forever.fantasy27@gmail.com>
2017-03-06 23:38:38 +0800

    Update Dashboard & Add Layout

--- app/Dashboard/index.js
+++ app/Dashboard/index.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 
-import pkg from '../../package.json';
 import './style.scss';
 
 export default class Dashboard extends Component {
   render() {
     return (
       <div className="dashboard">
-        <header className="header">
-          <h1 className="brand">EnvMaster <small>{pkg.version}</small></h1>
-          <p className="description">{pkg.description}</p>
-        </header>
         <section>
-
+          <h2>Requirements</h2>
+          <ul>
+            <li>
+              <span>Homebrew</span>
+            </li>
+            <li>
+              <span>Node.js</span>
+            </li>
+          </ul>
         </section>
       </div>
     );
