f3046b54b4813f6b7c8f7703e0677c4ed3ad8d63
Fantasy Shao <forever.fantasy27@gmail.com>
2017-03-21 00:58:43 +0800

    feat: add content for about page

--- app/About/index.js
+++ app/About/index.js
@@ -3,7 +3,16 @@ import React from 'react';
 import './style.scss';
 
 export default () => (
-  <div className="about">
-    <h1>About</h1>
+  <div className="page">
+    <h1 className="page__title">About</h1>
+    <p className="page__paragraph">
+      EnvMaster is a tiny desktop application to make you build up your local development environment.
+    </p>
+    <p className="page__paragraph">
+      It is open-sourced on <a href="https://github.com/SFantasy/EnvMaster">Github</a>, any issues or pull request is welcomed.
+    </p>
+    <p className="page__footer">
+      Made with <a href="https://electron.atom.io/">Electron</a> and <a href="https://facebook.github.io/react">React</a>.
+    </p>
   </div>
 );
